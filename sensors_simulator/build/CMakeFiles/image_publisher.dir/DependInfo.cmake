3df065538a21bdc12c7514417fc011fd2ec363fc
AdrianoHRL <adrianohrl@unifei.edu.br>
2014-07-14 13:33:36 -0500

    Adding sensors_simulator package

---
+++ sensors_simulator/build/CMakeFiles/image_publisher.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viki/ros_workspace/sensors_simulator/src/image_publisher.cpp" "/home/viki/ros_workspace/sensors_simulator/build/CMakeFiles/image_publisher.dir/src/image_publisher.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
